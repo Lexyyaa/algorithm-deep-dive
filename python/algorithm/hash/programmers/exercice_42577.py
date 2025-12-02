7f394c18d029cc8f0b920e5092eef17afb4aabe4
Lexyyaa <shw9910274@gmail.com>
2025-12-02 22:59:52 +0900

    solve : 프로그래머스 코딩테스트 연습 - 전화번호목록(Lv.2)

--- python/algorithm/hash/programmers/exercice_42577.py
+++ python/algorithm/hash/programmers/exercice_42577.py
@@ -1,2 +1,17 @@
 ## https://github.com/Lexyyaa/algorithm-deep-dive.git
 
+#phone_book = ["119", "97674223", "1195524421"]
+#phone_book = ["123","456","789"]
+phone_book = ["12","123","1235","567","88"]
+
+def solution(phoneBook):
+    phoneBook.sort() ## 먼저 오름차순으로 정렬하고나서
+    i = 1
+    while i < len(phoneBook):
+        if phoneBook[i].startswith(phoneBook[i-1]):
+            return False
+        i += 1 ## 한칸씩 추가해가며 startswith 함수로 비교하기!
+    return True
+
+print(solution(phone_book))
+

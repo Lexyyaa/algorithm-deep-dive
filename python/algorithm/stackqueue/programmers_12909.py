75751fb082bc7cdef1f993cc6c0efb6d11fed205
Lexyyaa <shw9910274@gmail.com>
2025-12-12 15:18:30 +0900

    solve : 올바른 괄호

---
+++ python/algorithm/stackqueue/programmers_12909.py
@@ -0,0 +1,19 @@
+## https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+
+## # 올바른 괄호
+## 여는 괄호가 있으면 닫는괄호가 있어야하니까
+## 여는괄호를 조건으로 잡고 닫는괄호가 있으면 pop 하는 방식으로 진행 ㄱㄱ
+def solution(s):
+    stack = []
+
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
